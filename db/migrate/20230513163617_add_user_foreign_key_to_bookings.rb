bedfd59a3616a6c211d6c1e705f10292d928b99c
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-05-14 13:03:27 +0100

    rollback foreign_id migrations

--- db/migrate/20230513163617_add_user_foreign_key_to_bookings.rb
+++
@@ -1,5 +0,0 @@
-class AddUserForeignKeyToBookings < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :bookings, :users, null: false, foreign_key: true
-  end
-end
--- db/migrate/20230513163819_add_truck_foreign_key_to_bookings.rb
+++
@@ -1,5 +0,0 @@
-class AddTruckForeignKeyToBookings < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :bookings, :trucks, null: false, foreign_key: true
-  end
-end
