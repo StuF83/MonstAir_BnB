fa8eb75428a6f351f5ad6c8e73b3d73febfe1b19
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-05-14 13:17:49 +0100

    model relations working
    old migrations to add foreign keys to bookings table rolled back, new migrations created. Correct associations now in place

---
+++ db/migrate/20230514121234_add_truck_foreign_key_to_bookings.rb
@@ -0,0 +1,5 @@
+class AddTruckForeignKeyToBookings < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :bookings, :truck, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_13_154546) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_14_121234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_13_154546) do
     t.float "total_cost"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "truck_id"
+    t.index ["truck_id"], name: "index_bookings_on_truck_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "trucks", force: :cascade do |t|
@@ -45,4 +49,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_13_154546) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "trucks"
+  add_foreign_key "bookings", "users"
 end
