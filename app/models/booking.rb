dbc8a5000829f69b153a92716448a7cd5d9b713a
Omesh Modgill <omeshmodgill@hotmail.co.uk>
2023-05-16 20:09:09 +0100

    Booking Validations Added

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :truck
+
+  validates :total_cost, :start_date, :end_date, presence: true
+#  validates :start_date, comparison: { less_than: :end_date }
+#consider coming back to this
 end
