8fb93c90f4c439897a131fbe0ca483ac2b9e4fef
Stuart <108226770+StuF83@users.noreply.github.com>
2023-05-16 21:43:23 +0100

    Revert "Date Validation Sorted"

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,6 @@ class Booking < ApplicationRecord
   belongs_to :truck
 
   validates :total_cost, :start_date, :end_date, presence: true
-  validates :start_date, comparison: { less_than: :end_date }
+#  validates :start_date, comparison: { less_than: :end_date }
+#consider coming back to this
 end
