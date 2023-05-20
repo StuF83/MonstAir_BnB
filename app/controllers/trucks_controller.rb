0b3828f24213e7910e7d7fa213a23adc1a5cb763
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-05-20 10:13:35 +0100

    new and create truck actions set

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -12,4 +12,20 @@ class TrucksController < ApplicationController
     @trucks = Truck.where(location: params[:location].downcase, daily_fee: params[:daily_fee])
     render "index", locals: { trucks: @trucks }
   end
+
+  def new
+    @truck = Truck.new
+  end
+
+  def create
+    @truck = Truck.new(truck_params)
+    @truck.save
+    redirect_to truck_path(@truck)
+  end
+
+  private
+
+  def truck_params
+    params.require(:truck).permit(:name, :description, :daily_fee)
+  end
 end
