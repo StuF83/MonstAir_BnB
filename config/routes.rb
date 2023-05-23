d3858a8465c3fb7549e2a34790ee34bb94bf3b99
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-05-23 21:31:58 +0100

    sorted card text spacing on truck index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  # root "articles#index"
-  # get "booking/:id", to: "bookings#show"
-  get 'trucks/search', to: 'trucks#search', as: 'search_trucks'
 
-  # get "trucks/:id", to: "trucks#show", as: 'truck'
-  # get "trucks", to: "trucks#index"
+  get 'trucks/search', to: 'trucks#search', as: 'search_trucks'
+  get '/bookings/:booking_id/confirmation', to: 'bookings#confirmation', as: 'confirmation'
 
   resources :trucks do
-    resources :bookings
+    resources :bookings, only: [:new, :create, :show]
   end
 end
