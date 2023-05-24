7c09c7ce19edec59547b07bc427e1d8548752e83
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-05-24 07:38:46 +0100

    updated seed file with real truck names

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,16 @@
 require 'faker'
 
 cities = ["hackney", "dalston", "shorditch", "greenwhich", "enfield"]
+names = ["Bigfoot", "Grave Digger", "Maximum Destruction", "El Toro Loco", "Monster Mutt", "Zombie", "Megalodon", "Dragon", "EarthShaker, Son-uva Digger", "Bounty Hunter", "Avenger", "Iron Outlaw", "King Krunch", "Raminator", "Black Stallion", "Overkill Evolution", "Jurassic Attack", "Predator", "Gunslinger", "Madusa", "Scooby-Doo, Captain's Curse", "Mohawk Warrior", "Blue Thunder", "Gas Monkey Garage", "Northern Nightmare", "Storm Damage", "Bad Habit", "Backdraft"]
 fee = [100, 150, 200, 250, 300]
-30.times do
+28.times do
+  random_name = names.sample
   @truck = Truck.create(
-    name: Faker::Name.name,
+    name: random_name,
     description: Faker::Company.type,
     daily_fee: fee.sample,
     location: cities.sample
   )
-  puts "Name:#{@truck.name}\nDaily Fee:#{@truck.description}\nDaily Fee:#{@truck.daily_fee}\nLocation:#{@truck.location}"
+  names.delete(random_name)
+  puts "Name:#{@truck.name}\nDescription:#{@truck.description}\nDaily Fee:#{@truck.daily_fee}\nLocation:#{@truck.location}"
 end
