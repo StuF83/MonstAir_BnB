3da4cd55dd442cab105a244053d78455ad52d4a7
Omesh Modgill <omeshmodgill@hotmail.co.uk>
2023-05-25 18:53:23 +0100

    User Area PageStyling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,15 @@ class PagesController < ApplicationController
     @user = current_user
     @bookings = current_user.bookings
     @trucks = Truck.all
+    @id = @user.id
+    @rentals = []
+
+    @trucks.each do |truck|
+      if truck.owner_id == @id
+        @rentals << truck.name
+      end
+    end
+
+    @rented = @rentals
   end
 end
